86c66e20eca768ad0da9097fa61074a9aa08b306
David Kviloria <datokviloria@gmail.com>
2020-07-14 12:40:04 +0400

    pwa

--- node-app/next.config.js
+++ node-app/next.config.js
@@ -1,8 +1,19 @@
 const config = require("./shared/config");
+const withPWA = require("next-pwa");
 
-module.exports = {
+module.exports = withPWA({
 	excludeFile: (str) => /\*.{spec,test,config}.js/.test(str),
 	env: {
 		...config,
 	},
-};
+	pwa: {
+		dest: "public",
+	},
+});
+
+// module.exports = {
+// 	excludeFile: (str) => /\*.{spec,test,config}.js/.test(str),
+// 	env: {
+// 		...config,
+// 	},
+// };
--- node-app/pages/index.tsx
+++ node-app/pages/index.tsx
@@ -62,8 +62,22 @@ export default function Home() {
 	return (
 		<div className=" w-full h-screen overflow-x-scroll">
 			<Head>
-				<title>TinyHoods - Explore Tiny world</title>
 				<link rel="icon" href="/favicon.ico" />
+				<meta charSet="utf-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta name="description" content="Description" />
+				<meta name="keywords" content="Keywords" />
+				<title>TinyHoods - Explore Tiny world</title>
+
+				<link rel="manifest" href="/manifest.json" />
+				<link href="/location.svg" rel="icon" type="image/svg" sizes="16x16" />
+				<link href="/location.svg" rel="icon" type="image/svg" sizes="32x32" />
+				<link rel="apple-touch-icon" href="/location.svg"></link>
+				<meta name="theme-color" content="#317EFB" />
 			</Head>
 
 			{!loading && !error && (
