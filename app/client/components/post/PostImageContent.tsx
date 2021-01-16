576c0163afd2163fa354ea9a11ccfde1552d303f
David Kviloria <datokviloria@gmail.com>
2021-01-17 02:26:31 +0400

    dont optimize image

--- app/client/components/post/PostImageContent.tsx
+++ app/client/components/post/PostImageContent.tsx
@@ -15,6 +15,7 @@ export default function PostImageContent({
 			{images.map((each: any, imageIndex: number) => (
 				<div key={imageIndex} className="cursor-pointer p-1">
 					<Image
+						unoptimized
 						alt="Thumbnail"
 						className="rounded-md bg-secondary shadow-md"
 						src={
